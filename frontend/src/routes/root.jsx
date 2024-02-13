d155990de315ff0116be7700e0386a2572a498dd
Sam <burtman.samuel26@gmail.com>
2024-02-13 21:03:06 +0200

    add frontend/src/routes/

---
+++ frontend/src/routes/root.jsx
@@ -0,0 +1,64 @@
+import { Formik } from 'formik'
+
+const LoginPage = () => (
+	<div
+		style={{
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'center',
+			alignItems: 'center',
+			height: '100vh',
+		}}
+	>
+		<h1>Войти</h1>
+		<Formik
+			initialValues={{ email: '', password: '' }}
+			onSubmit={(values, { setSubmitting }) => {
+				setTimeout(() => {
+					alert(JSON.stringify(values, null, 2))
+					setSubmitting(false)
+				}, 400)
+			}}
+		>
+			{({
+				values,
+				errors,
+				touched,
+				handleChange,
+				handleBlur,
+				handleSubmit,
+				isSubmitting,
+			}) => (
+				<form
+					onSubmit={handleSubmit}
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<input
+						type='email'
+						name='email'
+						onChange={handleChange}
+						onBlur={handleBlur}
+						value={values.email}
+					/>
+					{errors.email && touched.email && errors.email}
+					<input
+						type='password'
+						name='password'
+						onChange={handleChange}
+						onBlur={handleBlur}
+						value={values.password}
+					/>
+					{errors.password && touched.password && errors.password}
+					<button type='submit' disabled={isSubmitting}>
+						Submit
+					</button>
+				</form>
+			)}
+		</Formik>
+	</div>
+)
+export default LoginPage
