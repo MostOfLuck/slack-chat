c95d2fdb6806a903fac558fbf4e6c0c1b33add9d
Sam <burtman.samuel26@gmail.com>
2024-02-16 15:13:52 +0200

    update frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import ErrorPage from './routes/error-page'
 import { default as LoginPage, default as Root } from './routes/loginPage'
 import Navigation from './routes/nav'
+import Reg from './routes/reg'
 
 const App = () => (
 	<Router>
@@ -20,6 +21,7 @@ const App = () => (
 			/>
 			<Route path='/login' element={<LoginPage />} />
 			<Route path='*' element={<ErrorPage />} />
+			<Route path='/register' element={<Reg />} />
 		</Routes>
 	</Router>
 )
