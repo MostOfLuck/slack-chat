5f1833398a22bb0c89f239dbce3cc837be177ee1
Sam <burtman.samuel26@gmail.com>
2024-02-17 23:02:34 +0200

    add frontend/src/slices/

---
+++ frontend/src/slices/index.js
@@ -0,0 +1,20 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+import channelsInfo, { actions as channelsInfoActions } from './channelsInfo.js'
+import messagesInfo, { actions as messagesInfoActions } from './messagesInfo.js'
+import modalInfo, { actions as modalInfoActions } from './modalInfo.js'
+
+const reducer = combineReducers({
+	channelsInfo,
+	messagesInfo,
+	modalInfo,
+})
+export const actions = {
+	...channelsInfoActions,
+	...messagesInfoActions,
+	...modalInfoActions,
+}
+
+export default configureStore({
+	reducer,
+})
---
+++ frontend/src/slices/messagesInfo.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit'
+
+import fetchData from '../requests/mergeData.js'
+import { actions as channelsInfoActions } from './channelsInfo.js'
+
+const { deleteChannel } = channelsInfoActions
+
+const slice = createSlice({
+	name: 'messagesInfo',
+	initialState: { messages: [] },
+	reducers: {
+		addMessage(state, { payload }) {
+			state.messages.push(payload)
+		},
+	},
+	extraReducers: builder => {
+		builder
+			.addCase(fetchData.fulfilled, (state, { payload }) => {
+				state.messages = payload.messages
+			})
+			.addCase(deleteChannel, (state, { payload }) => {
+				state.messages = state.messages.filter(
+					message => message.channelId !== payload.id
+				)
+			})
+	},
+})
+
+export const { actions } = slice
+export default slice.reducer
