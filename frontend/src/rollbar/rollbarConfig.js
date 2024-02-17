5132959a3f33e2564f58dc8c71bafe6aab6f4ca1
Sam <burtman.samuel26@gmail.com>
2024-02-17 23:02:07 +0200

    update frontend/src/rollbar/

---
+++ frontend/src/rollbar/rollbarConfig.js
@@ -0,0 +1,10 @@
+const rollbarConfig = {
+	accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+	payload: {
+		environment: 'production',
+	},
+	captureUncaught: true,
+	captureUnhandledRejections: true,
+}
+
+export default rollbarConfig
