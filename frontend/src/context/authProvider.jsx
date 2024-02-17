aef562aa5271938aa906650044902def13afbcf8
Sam <burtman.samuel26@gmail.com>
2024-02-17 23:01:03 +0200

    update frontend/src/context/

---
+++ frontend/src/context/authProvider.jsx
@@ -0,0 +1,47 @@
+import React, { useCallback, useMemo, useState } from 'react'
+import { AuthContext } from './context.js'
+
+const AuthProvider = ({ children }) => {
+	const savedUserData = JSON.parse(localStorage.getItem('userId'))
+	const [loggedIn, setLoggedIn] = useState(Boolean(savedUserData))
+	const [user, setUser] = useState(
+		savedUserData ? { username: savedUserData.username } : null
+	)
+
+	const logIn = useCallback(userData => {
+		setLoggedIn(true)
+		setUser({ username: userData.username })
+	}, [])
+
+	const logOut = useCallback(() => {
+		localStorage.removeItem('userId')
+		setUser(null)
+		setLoggedIn(false)
+	}, [])
+
+	const getAuthHeader = useCallback(() => {
+		const userId = JSON.parse(localStorage.getItem('userId'))
+		if (userId && userId.token) {
+			return { Authorization: `Bearer ${userId.token}` }
+		}
+		logOut()
+		return {}
+	}, [logOut])
+
+	const providedData = useMemo(
+		() => ({
+			loggedIn,
+			logIn,
+			logOut,
+			getAuthHeader,
+			user,
+		}),
+		[loggedIn, logIn, logOut, getAuthHeader, user]
+	)
+
+	return (
+		<AuthContext.Provider value={providedData}>{children}</AuthContext.Provider>
+	)
+}
+
+export default AuthProvider
---
+++ frontend/src/context/chatApi.js
@@ -0,0 +1,68 @@
+import { io } from 'socket.io-client'
+
+import store, { actions } from '../slices/index.js'
+
+const { addMessage, addChannel, deleteChannel, channelRename } = actions
+const { dispatch } = store
+const socket = io()
+socket.on('newMessage', payload => {
+	dispatch(addMessage(payload))
+})
+socket.on('newChannel', payload => {
+	dispatch(addChannel(payload))
+})
+socket.on('removeChannel', payload => {
+	dispatch(deleteChannel(payload))
+})
+socket.on('renameChannel', payload => {
+	dispatch(channelRename(payload))
+})
+
+const chatApi = {
+	sendMessage: message =>
+		new Promise((resolve, reject) => {
+			socket.emit('newMessage', message, response => {
+				if (response.error) {
+					console.error(response.error)
+					reject()
+				} else {
+					resolve()
+				}
+			})
+		}),
+	newChannel: name =>
+		new Promise((resolve, reject) => {
+			socket.emit('newChannel', { name }, response => {
+				if (response.error) {
+					console.error(response.error)
+					reject()
+				} else {
+					resolve(response.data.id)
+				}
+			})
+		}),
+	removeChannel: id =>
+		new Promise((resolve, reject) => {
+			socket.emit('removeChannel', { id }, response => {
+				if (response.error) {
+					console.error(response.error)
+					reject()
+				} else {
+					resolve()
+				}
+			})
+		}),
+	renameChannel: ({ name, id }) =>
+		new Promise((resolve, reject) => {
+			socket.emit('renameChannel', { name, id }, response => {
+				if (response.error) {
+					console.error(response.error)
+					reject()
+				} else {
+					resolve()
+				}
+			})
+		}),
+}
+
+export default chatApi
