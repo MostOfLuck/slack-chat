c1da825b860e5192f2be976baf948aebb8ef895a
Sam <burtman.samuel26@gmail.com>
2024-02-17 23:01:15 +0200

    update frontend/src/hooks/

---
+++ frontend/src/hooks/hooks.js
@@ -0,0 +1,11 @@
+import { useContext } from 'react'
+
+import { AuthContext, chatApiContext } from '../context/context.js'
+
+const useAuth = () => useContext(AuthContext)
+const useChatApi = () => {
+	const chatApi = useContext(chatApiContext)
+	return chatApi
+}
+
+export { useAuth, useChatApi }
