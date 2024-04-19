4c4369d1a2b00850839ab711674d8a81213f14b1
MostOfLuck <burtman.samuel26@gmail.com>
2024-04-19 13:56:42 +0200

    update frontend/src/components/Login/LoginCard.jsx

--- frontend/src/components/Login/LoginCard.jsx
+++ frontend/src/components/Login/LoginCard.jsx
@@ -1,20 +1,25 @@
 import { Card, Col, Image } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import LoginForm from './LoginForm';
 import LoginFooter from './LoginFooter';
 import loginImg from '../../img/login.png';
 
-const LoginCard = () => (
-  <Col xs={12} md={8} xxl={6}>
-    <Card className="shadow-sm">
-      <Card.Body className="row p-5">
-        <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
-          <Image className="rounded-circle" with={200} height={200} alt="Войти" src={loginImg} />
-        </Col>
-        <LoginForm />
-      </Card.Body>
-      <LoginFooter />
-    </Card>
-  </Col>
-);
+const LoginCard = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Col xs={12} md={8} xxl={6}>
+      <Card className="shadow-sm">
+        <Card.Body className="row p-5">
+          <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
+            <Image className="rounded-circle" with={200} height={200} alt={t('loginTitle')} src={loginImg} />
+          </Col>
+          <LoginForm />
+        </Card.Body>
+        <LoginFooter />
+      </Card>
+    </Col>
+  );
+};
 
 export default LoginCard;
